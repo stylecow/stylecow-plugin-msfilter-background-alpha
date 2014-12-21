0e50bd20eb240d1c3d5cbcc33b044a09a3230913
oscarotero <oom@oscarotero.com>
2014-12-22 00:05:16 +0100

    api changes

--- index.js
+++ index.js
@@ -3,33 +3,32 @@ var color = require('stylecow-color');
 module.exports = function (stylecow) {
 
 	stylecow.addTask({
-		disable: {
+		forBrowsersLowerThan: {
 			explorer: 9.0
 		},
-		Declaration: {
-			background: fixer,
-			"background-color": fixer
+		filter: {
+			type: 'Declaration',
+			name: ['background', 'background-color']
+		},
+		fn: function (declaration) {
+			var fn = declaration.searchFirst({
+				type: 'Function',
+				name: ['rgba', 'hsla']
+			});
+
+			if (fn) {
+				var rgba = color.toRGBA(fn);
+
+				if (rgba[3] === 1) {
+					return fn.replaceWith(stylecow.Keyword.createFromString('#' + color.RGBA_HEX(rgba)));
+				}
+
+				var hex = '#' + Math.round(255 * rgba[3]).toString(16) + color.RGBA_HEX(rgba);
+				var filter = 'progid:DXImageTransform.Microsoft.gradient(startColorStr="' + hex + '", endColorStr="' + hex + '")';
+				var block = declaration.parent({type: 'Block' });
+
+				stylecow.utils.addMsFilter(block, filter);
+			}
 		}
 	});
-
-	function fixer (declaration) {
-		var fn = declaration.searchFirst({
-			type: 'Function',
-			name: ['rgba', 'hsla']
-		});
-
-		if (fn) {
-			var rgba = color.toRGBA(fn);
-
-			if (rgba[3] === 1) {
-				return fn.replaceWith(stylecow.Keyword.createFromString('#' + color.RGBA_HEX(rgba)));
-			}
-
-			var hex = '#' + Math.round(255 * rgba[3]).toString(16) + color.RGBA_HEX(rgba);
-			var filter = 'progid:DXImageTransform.Microsoft.gradient(startColorStr="' + hex + '", endColorStr="' + hex + '")';
-			var block = declaration.parent({type: 'Block' });
-
-			stylecow.utils.addMsFilter(block, filter);
-		}
-	}
 };
