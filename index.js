ba4e50d71267f267f0afac0e79118497c5ea3ab2
oscarotero <oom@oscarotero.com>
2015-09-10 12:49:41 +0200

    updated for stylecow-core v2

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
+"use strict";
+
 var Color = require('color');
 
-module.exports = function (stylecow) {
+module.exports = function (tasks) {
 
-	stylecow.addTask({
-		forBrowsersLowerThan: {
-			explorer: 9.0
-		},
-		filter: {
-			type: 'Declaration',
-			name: ['background', 'background-color']
-		},
-		fn: function (declaration) {
-			var fn = declaration.get({
-				type: 'Function',
-				name: ['rgba', 'hsla']
-			});
+    tasks.addTask({
+        forBrowsersLowerThan: {
+            explorer: 9.0
+        },
+        filter: {
+            type: 'Declaration',
+            name: ['background', 'background-color']
+        },
+        fn: function (declaration) {
+            var fn = declaration.get({
+                type: 'Function',
+                name: ['rgba', 'hsla']
+            });
 
-			if (fn) {
-				var color = Color(fn.toString());
-				var hex = color.hexString();
+            if (fn) {
+                let color = Color(fn.toString()),
+                    hex = color.hexString();
 
-				if (color.alpha() == 1) {
-					return fn.replaceWith(stylecow.parse(hex, 'Hex'));
-				}
+                if (color.alpha() == 1) {
+                    return fn.replaceWithCode(hex, 'Hex');
+                }
 
-				hex = hex.replace('#', '#' + Math.round(255 * color.alpha()).toString(16));
+                hex = hex.replace('#', '#' + Math.round(255 * color.alpha()).toString(16));
 
-				addMsFilter(declaration.getParent('Block'), 'progid:DXImageTransform.Microsoft.gradient(startColorStr="' + hex + '", endColorStr="' + hex + '")');
-			}
-		}
-	});
+                addMsFilter(declaration.getAncestor('Block'), 'progid:DXImageTransform.Microsoft.gradient(startColorStr="' + hex + '", endColorStr="' + hex + '")');
+            }
+        }
+    });
 
-	function addMsFilter (block, filter) {
-		var declaration = block.getChild({
-				type: 'Declaration',
-				name: 'filter',
-				vendor: 'ms'
-			});
+    function addMsFilter (block, filter) {
+        var declaration = block.getChild({
+                type: 'Declaration',
+                name: 'filter',
+                vendor: 'ms'
+            });
 
-		if (!declaration) {
-			return block.push(stylecow.parse('-ms-filter: ' + filter, 'Declaration', 'createMsFilter'));
-		}
+        if (!declaration) {
+            return block.pushCode('-ms-filter: ' + filter, 'Declaration', 'createMsFilter');
+        }
 
-		if (declaration.is({string: '-ms-filter: none;'})) {
-			return declaration
-				.get({
-					type: 'Keyword',
-					name: 'none'
-				})
-				.replaceWith((new stylecow.String()).setName(filter));
-		}
+        if (declaration.is({string: '-ms-filter: none;'})) {
+            return declaration
+                .get({
+                    type: 'Keyword',
+                    name: 'none'
+                })
+                .replaceWith((new stylecow.String()).setName(filter));
+        }
 
-		var string = declaration.get('String');
+        var string = declaration.get('String');
 
-		if (string.name) {
-			string.name += ',' + filter;
-		} else {
-			string.name = filter;
-		}
-	}
+        if (string.name) {
+            string.name += ',' + filter;
+        } else {
+            string.name = filter;
+        }
+    }
 };
